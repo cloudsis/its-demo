30e7eba9d4c856b7d4e60867a74c90b601b03033
cloudsis <cloud@sis.seino.co.jp>
2013-10-16 20:48:11 +0900

    グラフ表示

--- ITS-demo/src/com/example/its_demo/DiagnosisActivity.java
+++ ITS-demo/src/com/example/its_demo/DiagnosisActivity.java
@@ -1,22 +1,63 @@
 package com.example.its_demo;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import jp.co.seino.sis.chart.EcoChartView;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.view.Menu;
 
 public class DiagnosisActivity extends Activity {
 
+
+	Handler handler = new Handler();
+	EcoChartView ecoChartView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_diagnosis);
+		//更新テスト
+		//あいうえお
+        // 初期化
+		ecoChartView = (EcoChartView)findViewById(R.id.diaChartView);
+        // グラフに表示するデータを生成
+        //int[] dataList = {10, 5, 20, 8};
+        //ecoChartView.createChart(dataList);
+		double[] dataList = {12.2,13.4,20.0,10.5,25.5};
+		double avg = 18.6;
+	    ecoChartView.createEcoChart(dataList,avg);
+	    
+	    // タイマーを生成
+	    Timer timer = new Timer(false);
+	    // スケジュールを設定
+	    timer.schedule(new TimerTask() {
+	    	@Override
+	    	public void run() {
+	    		handler.post(new Runnable() {
+	    			@Override
+	    			public void run() {
+	    				// 描画処理を指示
+	    				drawChart();
+	    			}
+	    		});
+	    	}
+	    }, 10000, 10000); // 初回起動の遅延(5sec)と周期(1sec)指定
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.diagnosis, menu);
 		return true;
+	}
+	
+	private void drawChart() {
+		System.out.println("drawChart!!!!");
+		double[]  dataList = {12.2,13.4};
+		ecoChartView.addData(dataList);
 	}
 
 }
--- ITS-demo/src/jp/co/seino/sis/chart/EcoChartView.java
+++ ITS-demo/src/jp/co/seino/sis/chart/EcoChartView.java
@@ -4,14 +4,20 @@ import org.afree.chart.AFreeChart;
 import org.afree.chart.ChartFactory;
 import org.afree.chart.StandardChartTheme;
 import org.afree.chart.axis.NumberAxis;
+import org.afree.chart.plot.CategoryPlot;
 import org.afree.chart.plot.PlotOrientation;
+import org.afree.chart.plot.ValueMarker;
 import org.afree.chart.plot.XYPlot;
 import org.afree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.afree.data.category.DefaultCategoryDataset;
 import org.afree.data.xy.XYSeries;
 import org.afree.data.xy.XYSeriesCollection;
+import org.afree.graphics.PaintType;
+import org.afree.graphics.SolidColor;
+import org.afree.util.PaintTypeUtilities;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
 
 public class EcoChartView extends ChartView {
@@ -20,45 +26,46 @@ public class EcoChartView extends ChartView {
 		super(context,attrs);
 	}
 
-    /**
-     * グラフ生成
-     */
-    public void createChart(int[] dataList) {
-        // X軸の定義
-        NumberAxis domainAxis = new NumberAxis("時間");
-        // Y軸の定義
-        NumberAxis rangeAxis = new NumberAxis("燃費km/l");
-        // 折れ線の定義
-        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-        // データのセット
-        XYSeries xy = new XYSeries("データ");
-        int i = 0;
-        for (int value : dataList) {
-            xy.add(i + 1, value);
-            i++;
-        }
-        XYSeriesCollection dataset = new XYSeriesCollection(xy);
-        XYPlot plot = new XYPlot(dataset, domainAxis, rangeAxis, renderer);
-
-        AFreeChart aFreeChart = new AFreeChart(plot);
-        setChart(aFreeChart);
-    }
+	private DefaultCategoryDataset dataset;
 
     /**
      * グラフ生成
      */
     public void createEcoChart(double[] dataList, double avg) {
+    	
     	ChartFactory.setChartTheme(StandardChartTheme.createLegacyTheme());
 
-    	DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+    	dataset = new DefaultCategoryDataset();
         int i = 0;
         for (double value : dataList) {
         	String cols = Integer.toString(i+1);
         	dataset.addValue(value, "燃費", cols);
-        	dataset.addValue(avg, "平均燃費", cols);
             i++;
         }
-        AFreeChart chart = ChartFactory.createLineChart("燃費グラフ", "時間", "燃費(km/l)", dataset, PlotOrientation.VERTICAL, true, false, false);
+        AFreeChart chart = ChartFactory.createBarChart("燃費グラフ", "時間", "燃費(km/l)", dataset, PlotOrientation.VERTICAL, true, false, false);
         setChart(chart);
+        
+        CategoryPlot plot = chart.getCategoryPlot();
+        
+        // 外枠の設定
+        plot.setOutlineVisible(false);
+        
+        //基準線を引く
+        ValueMarker marker = new ValueMarker(avg);
+        marker.setPaintType(new SolidColor(Color.GREEN));
+        marker.setStroke(2.0F);
+        marker.setLabel("平均燃費");
+        plot.addRangeMarker(marker);
+        
+    }
+    
+    public void addData(double[] dataList) {
+    	if (dataset == null) return;
+        int i = dataset.getColumnCount();
+        for (double value : dataList) {
+        	String cols = Integer.toString(i+1);
+        	dataset.addValue(value, "燃費", cols);
+            i++;
+        }
     }
 }
